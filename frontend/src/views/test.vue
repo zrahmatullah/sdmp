ecb72b96d1281f298c76252b4e998f4fd119e37f
Azhar As Rahmtulloh <83581979+zrahmatullah@users.noreply.github.com>
2024-11-19 10:45:23 +0700

    tambah pegawai fe azhar

--- frontend/src/views/test.vue
+++ frontend/src/views/test.vue
@@ -39,77 +39,118 @@
         </template>
       </Toolbar>
 
-     <DataTable
-      ref="dt"
-      :value="listdata"
-      v-model:selection="selectedProducts"
-      dataKey="id"
-      :first="first.value"
-      @page="onPageChange"
-      :paginator="true"
-      :rows="10"
-      :filters="filters"
-      paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
-      :rowsPerPageOptions="[5, 10, 25]"
-      currentPageReportTemplate="Showing {first} to {last} of {totalRecords} products"
-    >
-      <template #header>
-        <div class="flex flex-wrap gap-2 align-items-center justify-content-between">
-          <h4 class="m-0">Data Pegawai</h4>
-          <IconField iconPosition="left">
-            <InputIcon>
-              <i class="pi pi-search" />
-            </InputIcon>
-            <InputText v-model="filters['global'].value" placeholder="Search..." />
-          </IconField>
-        </div>
-      </template>
-
-      <Column selectionMode="multiple" style="width: 3rem" :exportable="false"></Column>
-      <Column 
-        header="No" 
-        style="width: 5rem;" 
+      <DataTable
+        ref="dt"
+        :value="listdata"
+        v-model:selection="selectedProducts"
+        dataKey="id"
+        :first="first.value"
+        @page="onPageChange"
+        :paginator="true"
+        :rows="10"
+        :filters="filters"
+        paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+        :rowsPerPageOptions="[5, 10, 25]"
+        currentPageReportTemplate="Showing {first} to {last} of {totalRecords} products"
       >
-        <template #body="slotProps">
-          {{ slotProps.index + first + 1 }}
+        <template #header>
+          <div
+            class="flex flex-wrap gap-2 align-items-center justify-content-between"
+          >
+            <h4 class="m-0">Data Pegawai</h4>
+            <IconField iconPosition="left">
+              <InputIcon>
+                <i class="pi pi-search" />
+              </InputIcon>
+              <InputText
+                v-model="filters['global'].value"
+                placeholder="Search..."
+              />
+            </IconField>
+          </div>
         </template>
-      </Column>
-      <Column field="namaPegawai" header="Nama Lengkap" sortable style="min-width: 12rem"></Column>
-      <Column field="email" header="Email" sortable style="min-width: 16rem"></Column>
-      <Column header="Image" >
-        <template #body="slotProps" >
-          <img
-            :src="slotProps.data.foto"
-            :alt="slotProps.data.namaPegawai"
-            class="border-round"
-            style="width: 100px;height: 100px;object-fit: cover;"
-          />
-        </template>
-      </Column>
-      <Column field="noTelepon" header="No Telepon" sortable style="min-width: 8rem"></Column>
-      <Column field="tglLahir" header="Tgl Lahir" sortable style="min-width: 10rem"></Column>
-      <Column field="tglGabung" header="Tgl Gabung" sortable style="min-width: 10rem"></Column>
-      <Column field="namaJabatan" header="Jabatan" sortable style="min-width: 12rem"></Column>
-      <Column field="namaDepartemen" header="Departemen" sortable style="min-width: 12rem"></Column>
-      <Column :exportable="false" style="min-width: 8rem">
-        <template #body="slotProps">
-          <Button
-            icon="pi pi-pencil"
-            outlined
-            rounded
-            class="mr-2"
-            @click="editProduct(slotProps.data)"
-          />
-          <Button
-            icon="pi pi-trash"
-            outlined
-            rounded
-            severity="danger"
-            @click="confirmDeleteProduct(slotProps.data)"
-          />
-        </template>
-      </Column>
-    </DataTable>
+
+        <Column
+          selectionMode="multiple"
+          style="width: 3rem"
+          :exportable="false"
+        ></Column>
+        <Column header="No" style="width: 5rem">
+          <template #body="slotProps">
+            {{ slotProps.index + first + 1 }}
+          </template>
+        </Column>
+        <Column
+          field="namaPegawai"
+          header="Nama Lengkap"
+          sortable
+          style="min-width: 12rem"
+        ></Column>
+        <Column
+          field="email"
+          header="Email"
+          sortable
+          style="min-width: 16rem"
+        ></Column>
+        <Column header="Image">
+          <template #body="slotProps">
+            <img
+              :src="slotProps.data.foto"
+              :alt="slotProps.data.namaPegawai"
+              class="border-round"
+              style="width: 100px; height: 100px; object-fit: cover"
+            />
+          </template>
+        </Column>
+        <Column
+          field="noTelepon"
+          header="No Telepon"
+          sortable
+          style="min-width: 8rem"
+        ></Column>
+        <Column
+          field="tglLahir"
+          header="Tgl Lahir"
+          sortable
+          style="min-width: 10rem"
+        ></Column>
+        <Column
+          field="tglGabung"
+          header="Tgl Gabung"
+          sortable
+          style="min-width: 10rem"
+        ></Column>
+        <Column
+          field="namaJabatan"
+          header="Jabatan"
+          sortable
+          style="min-width: 12rem"
+        ></Column>
+        <Column
+          field="namaDepartemen"
+          header="Departemen"
+          sortable
+          style="min-width: 12rem"
+        ></Column>
+        <Column :exportable="false" style="min-width: 8rem">
+          <template #body="slotProps">
+            <Button
+              icon="pi pi-pencil"
+              outlined
+              rounded
+              class="mr-2"
+              @click="editProduct(slotProps.data)"
+            />
+            <Button
+              icon="pi pi-trash"
+              outlined
+              rounded
+              severity="danger"
+              @click="confirmDeleteProduct(slotProps.data)"
+            />
+          </template>
+        </Column>
+      </DataTable>
     </div>
 
     <!-- CREATE DATA -->
@@ -125,7 +166,7 @@
           <label for="name">Nama Lengkap</label>
           <InputText
             id="name"
-            v-model.trim="data.namaPegawai "
+            v-model.trim="data.namaPegawai"
             required="true"
             autofocus
             :invalid="submitted && !data.namaPegawai"
@@ -142,7 +183,9 @@
             required="true"
             :invalid="submitted && !data.email"
           />
-          <small class="p-error" v-if="submitted && !data.email">Email diperlukan.</small>
+          <small class="p-error" v-if="submitted && !data.email"
+            >Email diperlukan.</small
+          >
         </div>
         <div class="field col">
           <label for="noHp">No Handphone</label>
@@ -194,73 +237,75 @@
           cols="30"
           :invalid="submitted && !data.alamat"
         />
-        <small class="p-error" v-if="submitted && !data.alamat">Alamat diperlukan.</small>
-      </div>
-      <div class="formgrid grid">
-      <div class="field col">
-        <label for="jabatan">Jabatan</label>
-        <Dropdown
-          id="jabatan"
-          v-model="data.jabatan"
-          :options="jabatanData"
-          optionLabel="label"
-          optionValue="value"
-          placeholder="Pilih Jabatan"
-          :invalid="submitted && !data.jabatan"
-        />
-        <small class="p-error" v-if="submitted && !data.jabatan"
-          >Jabatan diperlukan.</small
+        <small class="p-error" v-if="submitted && !data.alamat"
+          >Alamat diperlukan.</small
         >
       </div>
-      <div class="field col">
-        <label for="jabatan">Departemen</label>
-        <Dropdown
-          id="jabatan"
-          v-model="data.departemen"
-          :options="departemenData"
-          optionLabel="label"
-          optionValue="value"
-          placeholder="Pilih Jabatan"
-          :invalid="submitted && !data.departemen"
-        />
-       <small class="p-error" v-if="submitted && !data.departemen"
-          >Departemen diperlukan.</small
-        >
-      </div>
-      </div>
-      
       <div class="formgrid grid">
         <div class="field col">
-          <label for="foto">Upload Foto</label>
-          <div class="imgOld">
-            <img :src="fotoPreview" alt="Preview Foto" class="preview-foto" 
-          v-if="fotoPreview" />
-            <div >
-              <span class="nameFile">{{namafile}}</span>
-              <Button label="Foto lama" class="buttonNotif" />
-            </div>
-          </div>
-          <!-- <FileUpload @select="onFileSelect" accept="image/*" chooseLabel="Pilih Gambar"  :auto="false" :showUploadButton="false" :showCancelButton="false"> -->
-          <Toast />
-          <FileUpload
-            ref="fileUpload"
-            name="foto"
-            accept="image/*"
-            :custom-upload="true"
-            :auto="false"
-            @select="onFileSelect"
-            :showUploadButton="false"
-            :showCancelButton="false"
+          <label for="jabatan">Jabatan</label>
+          <Dropdown
+            id="jabatan"
+            v-model="data.jabatan"
+            :options="jabatanData"
+            optionLabel="label"
+            optionValue="value"
+            placeholder="Pilih Jabatan"
+            :invalid="submitted && !data.jabatan"
+          />
+          <small class="p-error" v-if="submitted && !data.jabatan"
+            >Jabatan diperlukan.</small
           >
-            <template #empty>
-              <p>Belum ada gambar yang diinput.</p>
-            </template>
-          </FileUpload>
+        </div>
+        <div class="field col">
+          <label for="jabatan">Departemen</label>
+          <Dropdown
+            id="jabatan"
+            v-model="data.departemen"
+            :options="departemenData"
+            optionLabel="label"
+            optionValue="value"
+            placeholder="Pilih Jabatan"
+            :invalid="submitted && !data.departemen"
+          />
+          <small class="p-error" v-if="submitted && !data.departemen"
+            >Departemen diperlukan.</small
+          >
+        </div>
+      </div>
+
+      <div class="form-container">
+        <!-- Kolom kiri untuk upload foto -->
+        <div class="form-section">
+          <h3>Upload Foto Baru</h3>
+          <FileUpload
+            mode="basic"
+            name="demo[]"
+            auto
+            chooseLabel="Pilih Foto"
+            accept="image/*"
+            customUpload
+            @upload="uploadHandler"
+          />
+          <p v-if="!selectedFile">Belum ada gambar yang dipilih.</p>
+        </div>
+
+        <!-- Kolom kanan untuk menampilkan foto lama -->
+        <div class="form-section">
+          <h3>Foto Lama</h3>
+          <div v-if="photoUrl" class="image-container">
+            <img :src="photoUrl" alt="Foto Lama" />
+          </div>
+          <p v-else>Belum ada foto lama.</p>
         </div>
       </div>
       <template #footer>
-        <Button label="Cancel" icon="pi pi-times" text @click="hideDialog" />
-        <Button label="Save" icon="pi pi-check" text @click="savePegawai" />
+        <button class="btn btn-cancel" @click="hideDialog">
+          <i class="pi pi-trash"></i> Batal
+        </button>
+        <button class="btn btn-save" @click="savePegawai">
+          <i class="pi pi-check"></i> Simpan
+        </button>
       </template>
     </Dialog>
 
@@ -278,7 +323,12 @@
         >
       </div>
       <template #footer>
-        <Button label="No" icon="pi pi-times" text @click="deleteProductDialog = false" />
+        <Button
+          label="No"
+          icon="pi pi-times"
+          text
+          @click="deleteProductDialog = false"
+        />
         <Button label="Yes" icon="pi pi-check" text @click="deleteProduct" />
       </template>
     </Dialog>
@@ -291,7 +341,9 @@
     >
       <div class="confirmation-content">
         <i class="pi pi-exclamation-triangle mr-3" style="font-size: 2rem" />
-        <span v-if="product">Are you sure you want to delete the selected products?</span>
+        <span v-if="product"
+          >Are you sure you want to delete the selected products?</span
+        >
       </div>
       <template #footer>
         <Button
@@ -300,7 +352,12 @@
           text
           @click="deleteProductsDialog = false"
         />
-        <Button label="Yes" icon="pi pi-check" text @click="deleteSelectedProducts" />
+        <Button
+          label="Yes"
+          icon="pi pi-check"
+          text
+          @click="deleteSelectedProducts"
+        />
       </template>
     </Dialog>
   </div>
@@ -347,12 +404,16 @@ const statuses = ref([
 const first = ref(0);
 
 const onPageChange = (event) => {
-  console.log('data event',event)
-  first.value = event.first; 
+  console.log("data event", event);
+  first.value = event.first;
 };
 
 const formatCurrency = (value) => {
-  if (value) return value.toLocaleString("en-US", { style: "currency", currency: "USD" });
+  if (value)
+    return value.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
   return;
 };
 const openNew = () => {
@@ -397,22 +458,26 @@ const onFileSelect = (event) => {
 };
 
 const savePegawai = async () => {
-
-  let id_data = pegawai_id.value ? pegawai_id.value : '';
-
+  let id_data = pegawai_id.value ? pegawai_id.value : "";
 
   const formData = new FormData();
   formData.append("foto", data.value.foto);
-  const formattedTglLahir = new Date(tglLahir.value).toLocaleDateString("id-ID", {
-    day: "2-digit",
-    month: "2-digit",
-    year: "numeric",
-  });
-  const formattedTglGabung = new Date(tglGabung.value).toLocaleDateString("id-ID", {
-    day: "2-digit",
-    month: "2-digit",
-    year: "numeric",
-  });
+  const formattedTglLahir = new Date(tglLahir.value).toLocaleDateString(
+    "id-ID",
+    {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    }
+  );
+  const formattedTglGabung = new Date(tglGabung.value).toLocaleDateString(
+    "id-ID",
+    {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    }
+  );
   formData.append("id_pegawai", id_data);
   formData.append("namaPegawai", data.value.namaPegawai);
   formData.append("emailPGW", data.value.email);
@@ -420,8 +485,16 @@ const savePegawai = async () => {
   formData.append("TglLahirPGW", formattedTglLahir);
   formData.append("TglGabungPGW", formattedTglGabung);
   formData.append("alamatPGW", data.value.alamat);
-  formData.append("jabatanPGW", data.value.jabatan.value ? data.value.jabatan.value : data.value.jabatan);
-  formData.append("departemenPGW", data.value.departemen.value ? data.value.departemen.value : data.value.departemen);
+  formData.append(
+    "jabatanPGW",
+    data.value.jabatan.value ? data.value.jabatan.value : data.value.jabatan
+  );
+  formData.append(
+    "departemenPGW",
+    data.value.departemen.value
+      ? data.value.departemen.value
+      : data.value.departemen
+  );
   console.log("data formData", formData);
   try {
     const response = await apiClient.post("/api/pegawai", formData, {
@@ -436,26 +509,30 @@ const savePegawai = async () => {
     console.log(response);
   } catch (error) {
     console.error("Gagal membuat data:", error);
-    toast.add({ severity: "error", summary: "Error", detail: "Gagal menyimpan data" });
+    toast.add({
+      severity: "error",
+      summary: "Error",
+      detail: "Gagal menyimpan data",
+    });
   }
 };
 
 const editProduct = (prod) => {
-  console.log('data prod',prod)
-  pegawai_id.value = prod.id
-  data.value.namaPegawai = prod.namaPegawai
-  data.value.email = prod.email
-  data.value.noHp = prod.noTelepon
-  tglLahir.value = prod.tglLahir
-  tglGabung.value = prod.tglGabung
-  data.value.alamat = prod.alamat
-  data.value.jabatan = prod.jabatan_id
-  data.value.departemen = prod.departemen_id
-  data.value.foto = prod.foto; 
+  console.log("data prod", prod);
+  pegawai_id.value = prod.id;
+  data.value.namaPegawai = prod.namaPegawai;
+  data.value.email = prod.email;
+  data.value.noHp = prod.noTelepon;
+  tglLahir.value = prod.tglLahir;
+  tglGabung.value = prod.tglGabung;
+  data.value.alamat = prod.alamat;
+  data.value.jabatan = prod.jabatan_id;
+  data.value.departemen = prod.departemen_id;
+  data.value.foto = prod.foto;
   fotoPreview.value = prod.foto;
-  const fileName = prod.foto.split('/').pop(); 
+  const fileName = prod.foto.split("/").pop();
   namafile.value = fileName;
- 
+
   productDialog.value = true;
 };
 const confirmDeleteProduct = (prod) => {
@@ -499,17 +576,16 @@ const confirmDeleteSelected = () => {
   if (selectedProducts.value.length > 0) {
     deleteProductsDialog.value = true;
   } else {
-    alert('No products selected for deletion');
+    alert("No products selected for deletion");
   }
 };
 const deleteSelectedProducts = async () => {
   try {
     const idsToDelete = selectedProducts.value.map((product) => product.id);
-    await apiClient.post('/api/pegawai', { delete: { pegawai_id: idsToDelete } });
-  } catch (error) {
-   
-    
-  }
+    await apiClient.post("/api/pegawai", {
+      delete: { pegawai_id: idsToDelete },
+    });
+  } catch (error) {}
 };
 
 const getStatusLabel = (status) => {
@@ -534,16 +610,15 @@ onMounted(() => {
 </script>
 
 <style scoped>
-
 .preview-foto {
-  width: 50px; 
-  height: 55px; 
+  width: 50px;
+  height: 55px;
   border: 1px solid #ddd;
-  object-fit: cover; 
+  object-fit: cover;
   margin-right: 10px;
 }
 
-.imgOld{
+.imgOld {
   border: 1px solid rgba(255, 250, 250, 0.705);
   display: flex;
   object-fit: cover;
@@ -551,18 +626,83 @@ onMounted(() => {
   text-align: center;
   margin: 15px;
   padding: 10px;
-
 }
-.nameFile{
+.nameFile {
   padding-right: 10px;
 }
-.buttonNotif{
+.buttonNotif {
   width: 100px;
   height: 20px;
 }
 
+.form-container {
+  display: flex;
+  gap: 20px;
+}
+
+.form-section {
+  flex: 1;
+  padding: 20px;
+  border-radius: 8px;
+  color: #fff;
+}
+
+.image-container {
+  width: 100%;
+  padding-top: 75%; /* Untuk rasio 4x3 */
+  position: relative;
+  background: #2c2c2c;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+.image-container img {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 4px;
+}
+
+.btn {
+  display: inline-flex;
+  align-items: center;
+  padding: 8px 12px;
+  font-size: 14px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.3s ease, opacity 0.3s ease;
+}
+
+.btn i {
+  margin-right: 8px;
+}
+
+.btn-cancel {
+  background-color: #e74c3c; /* Warna merah */
+  color: #fff;
+}
+
+.btn-cancel:hover {
+  background-color: #c0392b;
+}
+
+.btn-save {
+  background-color: #2ecc71; /* Warna hijau */
+  color: #fff;
+}
+
+.btn-save:hover {
+  background-color: #27ae60;
+}
+
+.btn:focus {
+  outline: none;
+}
 </style>
-
-
 
 ../services/ProductService
